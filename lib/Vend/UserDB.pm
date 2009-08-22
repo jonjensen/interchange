f29004086ec9a8eb6fe08bc87e42f42e0d13fb2f
Jon Jensen <jon@endpoint.com>
2009-08-22 17:47:22 -0600

    Prophylactically save $@ error in case cache() or something else later resets it

--- lib/Vend/UserDB.pm
+++ lib/Vend/UserDB.pm
@@ -1,8 +1,6 @@
 # Vend::UserDB - Interchange user database functions
 #
-# $Id: UserDB.pm,v 2.66 2009-05-01 13:50:01 pajamian Exp $
-#
-# Copyright (C) 2002-2008 Interchange Development Group
+# Copyright (C) 2002-2009 Interchange Development Group
 # Copyright (C) 1996-2002 Red Hat, Inc.
 #
 # This program is free software; you can redistribute it and/or modify
@@ -17,7 +15,7 @@
 
 package Vend::UserDB;
 
-$VERSION = substr(q$Revision: 2.66 $, 10);
+$VERSION = '2.67';
 
 use vars qw!
 	$VERSION
@@ -1957,15 +1955,16 @@ sub new_account {
 						);
 		}
 	};
+	my $err = $@;
 
 	scrub();
 
-	if($@) {
+	if ($err) {
 		if(defined $self) {
-			$self->{ERROR} = $@;
+			$self->{ERROR} = $err;
 		}
 		else {
-			logError( "Vend::UserDB error: %s\n", $@ );
+			logError("Vend::UserDB error: %s\n", $err);
 		}
 		return undef;
 	}
